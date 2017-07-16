243890ae96634d4562efac12ced29a184d6a8fe9
Claudinei Perboni <cperbony@gmail.com>
2017-07-16 00:18:51 -0300

    Add project files.

---
+++ GestaoComercial.website/App_Start/BundleConfig.cs
@@ -0,0 +1,46 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace GestaoComercial.website
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/js/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/js/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/js/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            #region [Bootstrap]
+
+            bundles.Add(new ScriptBundle("~/js/bootstrap")
+                .Include("~/Scripts/bootstrap.js")
+                .Include("~/Scripts/respond.js")
+                );
+
+            bundles.Add(new StyleBundle("~/css/bootstrap")
+                .Include("~/Content/bootstrap.css")
+                .Include("~/Scripts/bootstrap-theme.css")
+                      );
+
+            #endregion
+
+
+
+            bundles.Add(new StyleBundle("~/css/site").Include(
+                      "~/Content/site.css")
+                      );
+
+            BundleTable.EnableOptimizations = true;
+
+        }
+    }
+}
---
+++ GestaoComercial.website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GestaoComercial.website.Startup))]
+namespace GestaoComercial.website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
